44a8c5332786edac932ea2ff848518381db87ee8
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-09-23 16:39:19 +0400

    1) add about.html, 2) add about page to urls.py 3) move logo.html to separate block 4) add about view in views.py

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render_to_response
 # Create your views here.
 def index(request):
     return render_to_response('main/index.html')
+
+
+def about(request):
+    return render_to_response('main/about.html')
