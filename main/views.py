22811c6a81af7fd348f7fef0d4df1b60e085f64b
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-09-29 01:06:05 +0400

    bind catalog.html to database via new CBV CatalogView also modify urls.py to work with CatalogView

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from .models import IndexPage, ContactInfo, ServicesPage, CatalogItem
-from django.views.generic import DetailView
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from .models import IndexPage, ContactInfo, ProjectsPage, CatalogItem
+from django.views.generic import DetailView, ListView
 
 
 # Create your views here.
@@ -8,13 +8,29 @@ def index(request):
     context = {
         'index_page': IndexPage.objects.first(),
         'contact': ContactInfo.objects.first(),
-        'service_objects': ServicesPage.objects.first().get_random_catalog_items(3)
+        'service_objects': ProjectsPage.objects.first().get_random_catalog_items(3)
     }
     return render_to_response('main/index.html', context=context)
 
 
 def about(request):
     return render_to_response('main/about.html')
+
+
+class CatalogView(ListView):
+    model = CatalogItem
+    template_name = 'main/catalog.html'
+    context_object_name = 'catalog_items'
+
+    def get_queryset(self):
+        if 'type' in self.kwargs:
+            return get_list_or_404(CatalogItem, type=self.kwargs['type'])
+        return CatalogItem.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CatalogView, self).get_context_data(**kwargs)
+        context['contact'] = ContactInfo.objects.first()
+        return context
 
 
 def catalog(request):
