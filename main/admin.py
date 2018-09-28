cb2ff9b6054d3435446548f66038daedb50acf46
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-09-29 01:17:44 +0400

    small refactor

--- main/admin.py
+++ main/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import IndexPage, ContactInfo, CatalogItem, ProjectsPage, CatalogItemImg
+from . import models as reroom_models
 
 
 class CatalogItemImgAdminInline(admin.StackedInline):
-    model = CatalogItemImg
+    model = reroom_models.CatalogItemImg
 
 
 class CatalogItemAdmin(admin.ModelAdmin):
     inlines = [CatalogItemImgAdminInline]
 
 
-admin.site.register(IndexPage)
-admin.site.register(ContactInfo)
-admin.site.register(ProjectsPage)
-admin.site.register(CatalogItem, CatalogItemAdmin)
+admin.site.register(reroom_models.IndexPage)
+admin.site.register(reroom_models.ContactInfo)
+admin.site.register(reroom_models.ProjectsPage)
+admin.site.register(reroom_models.CatalogItem, CatalogItemAdmin)
