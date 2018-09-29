1343bb6493806db241b9ddd4d43af9ab1790bb17
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-09-30 00:40:35 +0400

    add HeadingAndText as inline in ServicesPage

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericStackedInline
 from . import models as reroom_models
 
 
@@ -10,7 +11,17 @@ class CatalogItemAdmin(admin.ModelAdmin):
     inlines = [CatalogItemImgAdminInline]
 
 
+class HeadingAndTextInlineAdmin(GenericStackedInline):
+    model = reroom_models.HeadingAndText
+    extra = 1
+
+
+class ServicePageAdmin(admin.ModelAdmin):
+    inlines = [HeadingAndTextInlineAdmin]
+
+
 admin.site.register(reroom_models.IndexPage)
 admin.site.register(reroom_models.ContactInfo)
 admin.site.register(reroom_models.ProjectsPage)
 admin.site.register(reroom_models.CatalogItem, CatalogItemAdmin)
+admin.site.register(reroom_models.ServicesPage, ServicePageAdmin)
