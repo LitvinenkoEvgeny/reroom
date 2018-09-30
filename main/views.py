fd07efc3740ef3ecbef41c89bf5cc9b622634452
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-10-01 00:07:12 +0400

    create ServcesListView with GenericListView

--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
-from .models import IndexPage, ContactInfo, ProjectsPage, CatalogItem
+from . import models
 from django.views.generic import DetailView, ListView
 
 
 # Create your views here.
 def index(request):
     context = {
-        'index_page': IndexPage.objects.first(),
-        'contact': ContactInfo.objects.first(),
-        'service_objects': ProjectsPage.objects.first().get_random_catalog_items(3)
+        'index_page': models.IndexPage.objects.first(),
+        'contact': models.ContactInfo.objects.first(),
+        'service_objects': models.ProjectsPage.objects.first().get_random_catalog_items(3)
     }
     return render_to_response('main/index.html', context=context)
 
@@ -18,18 +18,18 @@ def about(request):
 
 
 class CatalogView(ListView):
-    model = CatalogItem
+    model = models.CatalogItem
     template_name = 'main/catalog.html'
     context_object_name = 'catalog_items'
 
     def get_queryset(self):
         if 'type' in self.kwargs:
-            return get_list_or_404(CatalogItem, type=self.kwargs['type'])
-        return CatalogItem.objects.all()
+            return get_list_or_404(models.CatalogItem, type=self.kwargs['type'])
+        return models.CatalogItem.objects.all()
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(CatalogView, self).get_context_data(**kwargs)
-        context['contact'] = ContactInfo.objects.first()
+        context['contact'] = models.ContactInfo.objects.first()
         return context
 
 
@@ -37,8 +37,23 @@ def catalog(request):
     return render_to_response('main/catalog.html')
 
 
-def services(request):
-    return render_to_response('main/services.html')
+# def services(request):
+#     return render_to_response('main/services_list.html')
+
+class ServicesListView(ListView):
+    model = models.ServicesItem
+    template_name = 'main/services_list.html'
+
+    def get_queryset(self):
+        if 'type' in self.kwargs:
+            return get_list_or_404(models.ServicesItem, type=self.kwargs['type'])
+        return models.ServicesItem.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ServicesListView, self).get_context_data(**kwargs)
+        context['contact'] = models.ContactInfo.objects.first()
+        context['services_page'] = models.ServicesPage.objects.first()
+        return context
 
 
 def design(request):
@@ -62,11 +77,11 @@ def contacts(request):
 
 
 class SingleItemView(DetailView):
-    model = CatalogItem
+    model = models.CatalogItem
     context_object_name = 'catalog_item'
     template_name = 'main/catalog_item.html'
 
     def get_context_data(self, **kwargs):
         context = super(SingleItemView, self).get_context_data(**kwargs)
-        context['contact'] = ContactInfo.objects.first()
+        context['contact'] = models.ContactInfo.objects.first()
         return context
