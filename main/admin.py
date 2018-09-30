228ef9229adbb572b8e93b160f8c06d14fa267ad
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-09-30 21:33:52 +0400

    add admin page related code for CatalogItem, ServicePage ServiceAccordion etc.

--- main/admin.py
+++ main/admin.py
@@ -16,8 +16,23 @@ class HeadingAndTextInlineAdmin(GenericStackedInline):
     extra = 1
 
 
+class CatalogItemInline(admin.StackedInline):
+    model = reroom_models.CatalogItem
+    extra = 3
+
+
 class ServicePageAdmin(admin.ModelAdmin):
     inlines = [HeadingAndTextInlineAdmin]
+
+
+class ServiceAccordionItemAdmin(admin.ModelAdmin):
+    inlines = [HeadingAndTextInlineAdmin]
+    extra = 1
+
+
+class ServiceItemAccordionInline(admin.StackedInline):
+    model = reroom_models.ServiceItemAccordion
+    extra = 0
 
 
 admin.site.register(reroom_models.IndexPage)
@@ -25,3 +40,5 @@ admin.site.register(reroom_models.ContactInfo)
 admin.site.register(reroom_models.ProjectsPage)
 admin.site.register(reroom_models.CatalogItem, CatalogItemAdmin)
 admin.site.register(reroom_models.ServicesPage, ServicePageAdmin)
+admin.site.register(reroom_models.ServiceItemAccordion, ServiceAccordionItemAdmin)
+admin.site.register(reroom_models.ServicesItem)
