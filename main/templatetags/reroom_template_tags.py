2cec53717bbb7cf0f9090c2a193cf9d028ca05c4
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-10-01 03:32:09 +0400

    add divide_by template tag

--- main/templatetags/reroom_template_tags.py
+++ main/templatetags/reroom_template_tags.py
@@ -8,4 +8,9 @@ def get_chunks(array, evenly_by):
         yield array[i:i + evenly_by]
 
 
+def divide_by(array, by):
+    return [array[i::by] for i in range(by)]
+
+
 register.filter('get_chunks', get_chunks)
+register.filter('divide_by', divide_by)
